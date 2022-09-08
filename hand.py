46ef72ac814d30895d85c65e4bb23c5368f1bf8f
Abu Salek <64322352+salek877@users.noreply.github.com>
2022-09-08 23:55:16 +0600

    Add files via upload

---
+++ hand.py
@@ -0,0 +1,36 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=2)
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+while True:
+    success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx,cy)
+                cv2.putText(img,str(id),(cx,cy),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,0,255),1)
+
+            mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img,str(int(fps)),(10,70), cv2.FONT_ITALIC,3,(0,255,0),3)
+
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
